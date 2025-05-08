d99c7abfdc4f294bf2e03332dc5ba14fd583a36b
benimmerman <ben.immerman@gmail.com>
2025-05-08 10:47:41 -0400

    reusable component spinner

---
+++ src/components/helpers/Spinner.jsx
@@ -0,0 +1,7 @@
+export const Spinner = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="mt-2 w-8 h-8 border-4 border-dark-purple border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  );
+};
