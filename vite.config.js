9a6dbae07593e59226b13a0e52191a043666526f
benimmerman <ben.immerman@gmail.com>
2025-05-09 15:33:25 -0400

    web manifest for pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [
+    tailwindcss(),
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["pwa-512x512.png", "pwa-192x192.png"],
+      manifest: {
+        name: "Trackly",
+        short_name: "Trackly",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          { src: "/pwa-192x192.png", sizes: "192x192", type: "image/png" },
+          { src: "/pwa-512x512.png", sizes: "512x512", type: "image/png" },
+        ],
+      },
+    }),
+  ],
 });
